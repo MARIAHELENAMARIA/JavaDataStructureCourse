ede6efb75605ad5c070e17a959d8c1c711456647
Maria Helena Baldan Lemes <101667718+MARIAHELENAMARIA@users.noreply.github.com>
2023-03-14 10:03:46 -0300

    Add files via upload

---
+++ br.com.projeto.no/src/ClassePrincipal.java
@@ -0,0 +1,21 @@
+public class ClassePrincipal {
+    public static void main(String[] args){
+        No no1 = new No("Conteudo no1");
+        No no2 = new No("conteudo no2");
+        No no3 = new No("conteudo no3");
+        No no4 = new No("conteudo no4");
+
+        no1.setProximoNo(no2);
+        no2.setProximoNo(no3);
+        no3.setProximoNo(no4);
+        
+        System.out.println(no1);
+
+        System.out.println(no1.getProximoNo());
+        System.out.println(no2.getProximoNo());
+        System.out.println(no3.getProximoNo());
+        System.out.println(no4.getProximoNo());
+
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+    }
+}
